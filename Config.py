7019dd246eb15de3c65f85fbc167e7e8ba3f916b
root <root@LAPTOP-R2FHBO1E.localdomain>
2025-12-06 18:51:19 +0800

    test

---
+++ Config.py
@@ -0,0 +1,65 @@
+import json, os
+from ncatbot.core import GroupMessage
+
+
+rootDir = os.getcwd()
+class Config:
+    def __init__(self):
+        self.cofig_json_path = os.path.join(rootDir, "config.json")
+        self.cofig={
+            "inited": False,
+            "images_path": {"pjsk":"image1","touhou":"image2","ATRI":"image3"}, 
+            "groups_id": []
+            }
+        self.config_to_show={    # 方便打印 
+            "pjsk":"是否开启pjsk图:     ",
+            "info_bvideo_words": "简介字数:  ",
+            "touhou":"是否开启随机东方图:  ",
+            "ATRI": "是否开启ATRI图:  "
+        }
+        if not os.path.exists(self.cofig_json_path):
+            with open(self.cofig_json_path, "w") as f:
+                f.write(json.dumps(self.config, indent=4))
+        with open(self.cofig_json_path, "r") as f:
+            self.config = json.load(f)
+        print("config:", self.config)
+
+    def output(self, group_id: int):
+        text ="这是一些设置：\n"
+        for key in self.config[str(group_id)]:
+            
+            #print(key, text)
+            #目前只有bool,int处理
+            if key not in self.config_to_show:
+                continue
+            text=text+self.config_to_show[key]
+            if type(self.config[str(group_id)][key])==bool:
+
+                    if self.config[str(group_id)][key]:
+                        text+="是"
+                    else:
+                        text+="否"
+            elif type(self.config[str(group_id)][key])==int:
+                    text+=str(self.config[str(group_id)][key])
+            #结束换行
+            text+="\n"
+        text =text[:-1]
+        return text
+    
+    #每个config的fun都要用
+    #用于判断群是否第一次出现
+    def detect_gruop_id(self, msg: GroupMessage):
+        if msg.group_id not in self.config["groups_id"]:
+            self.config[str(msg.group_id)]={     
+                #每个群的配置
+                #和config_to_show关键字一致
+                "pjsk":True,
+                "downloadbv":True,
+                "info_bvideo_words": 70,
+                "touhou":True,
+                "ATRI":True
+            }
+            self.config["groups_id"].append(msg.group_id)
+            #更新配置
+            with open(self.config_json_path, "w") as f:
+                f.write(json.dumps(self.config, indent=4))
